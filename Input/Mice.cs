d54bc1400751c8721647da63c89de5e5dae30f0f
Juan Antonio Cano <jacano@plainconcepts.com>
2018-10-04 19:33:42 +0200

    Progress

--- Input/Mice.cs
+++ Input/Mice.cs
@@ -48,52 +48,5 @@ namespace Avalonia.LinuxFramebuffer
                 }
             }
         }
-
-        static double TranslateAxis(input_absinfo axis, int value, double max)
-        {
-            return (value - axis.minimum) / (double) (axis.maximum - axis.minimum) * max;
-        }
-
-        private void ProcessEvent(EvDevDevice device, input_event ev)
-        {
-            if (ev.type == (short)EvType.EV_REL)
-            {
-                if (ev.code == (short) AxisEventCode.REL_X)
-                    _x = Math.Min(_width, Math.Max(0, _x + ev.value));
-                else if (ev.code == (short) AxisEventCode.REL_Y)
-                    _y = Math.Min(_height, Math.Max(0, _y + ev.value));
-                else
-                    return;
-                Event?.Invoke(new RawMouseEventArgs(null,
-                    0, RawMouseEventType.Move, new Point(_x, _y),
-                    InputModifiers.None));
-            }
-            if (ev.type ==(int) EvType.EV_ABS)
-            {
-                if (ev.code == (short) AbsAxis.ABS_X && device.AbsX.HasValue)
-                    _x = TranslateAxis(device.AbsX.Value, ev.value, _width);
-                else if (ev.code == (short) AbsAxis.ABS_Y && device.AbsY.HasValue)
-                    _y = TranslateAxis(device.AbsY.Value, ev.value, _height);
-                else
-                    return;
-                Event?.Invoke(new RawMouseEventArgs(null,
-                    0, RawMouseEventType.Move, new Point(_x, _y),
-                    InputModifiers.None));
-            }
-            if (ev.type == (short) EvType.EV_KEY)
-            {
-                RawMouseEventType? type = null;
-                if (ev.code == (ushort) EvKey.BTN_LEFT)
-                    type = ev.value == 1 ? RawMouseEventType.LeftButtonDown : RawMouseEventType.LeftButtonUp;
-                if (ev.code == (ushort)EvKey.BTN_RIGHT)
-                    type = ev.value == 1 ? RawMouseEventType.RightButtonDown : RawMouseEventType.RightButtonUp;
-                if (ev.code == (ushort) EvKey.BTN_MIDDLE)
-                    type = ev.value == 1 ? RawMouseEventType.MiddleButtonDown : RawMouseEventType.MiddleButtonUp;
-                if (!type.HasValue)
-                    return;
-
-                Event?.Invoke(new RawMouseEventArgs(null, 0, type.Value, new Point(_x, _y), default(InputModifiers)));
-            }
-        }
     }
 }
